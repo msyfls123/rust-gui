a4935a46b9d9baa7765052f49b2cef7c0e338054
msyfls123 <msyfls123@gmail.com>
2021-04-18 23:05:43 +0800

    use delegate

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,94 @@
-use druid::widget::{Button, Flex, Label};
-use druid::{AppLauncher, LocalizedString, PlatformError, Widget, WidgetExt, WindowDesc, Data, Lens};
+#![allow(irrefutable_let_patterns)]
+
+use druid::widget::{Button, Flex, Label };
+use druid::{
+    AppLauncher, LocalizedString, PlatformError,
+    Widget, WidgetExt, WindowDesc, Data, Lens, Target,
+    Selector, AppDelegate, DelegateCtx, Handled, Command, Env,
+};
 use reqwest::Error;
 use serde::{Deserialize};
-use std::sync::mpsc::{Sender, channel};
+use std::sync::mpsc::{Sender, channel, Receiver};
 use std::thread;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 #[serde(untagged)]
 enum Date {
     Int(u32),
     String(String),
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct Day {
     date: Date,
     content: String,
     suggestion: String,
 }
 
+const DAY_DATA: Selector<String> = Selector::new("day_data");
+
 #[derive(Clone, Data, Lens)]
 struct State {
     day: u32,
     #[data(ignore)]
     dispatch: Sender<u32>,
+    day_data: String,
+}
+
+struct Delegate;
+
+impl AppDelegate<State> for Delegate {
+    fn command(
+        &mut self,
+        _ctx: &mut DelegateCtx,
+        _target: Target,
+        cmd: &Command,
+        data: &mut State,
+        _env: &Env,
+    ) -> Handled {
+        if let Some(day) = cmd.get(DAY_DATA) {
+            data.day_data = day.to_string();
+            Handled::Yes
+        } else {
+            Handled::No
+        }
+    }
 }
 
 // #[tokio::main]
 fn main() -> Result<(), PlatformError> {
     let (tx, rx) = channel();
+    let main_window = WindowDesc::new(ui_builder);
+    let data = 0_u32;
+    let launcher = AppLauncher::with_window(main_window);
+    let event_sink = launcher.get_external_handle();
+    request_day(rx, event_sink);
+    launcher.use_simple_logger()
+        .delegate(Delegate {})
+        .launch(State {
+            day: data,
+            dispatch: tx.clone(),
+            day_data: String::from(""),
+        })
+
+}
+
+fn request_day(rx: Receiver<u32>, event_sink: druid::ExtEventSink) {
     thread::spawn(move || {
         while let res = rx.recv() {
             match res {
                 Ok(day) => {
                     println!("{}", day);
-                    thread::spawn(move || {
-                        let res = get_data(day).unwrap();
-                        println!("{:?}", res);
-                    });
+                    let res = get_data(day).unwrap();
+                    let day_data = format!("{:?}", res);
+                    event_sink
+                        .submit_command(DAY_DATA, day_data, Target::Auto)
+                        .expect("uhhh");
                 },
                 Err(err) => eprintln!("{:?}", err),
             }
         }
     });
-    let main_window = WindowDesc::new(ui_builder);
-    let data = 0_u32;
-    AppLauncher::with_window(main_window)
-        .use_simple_logger()
-        .launch(State {
-            day: data,
-            dispatch: tx.clone()
-        })
-
 }
 
 fn ui_builder() -> impl Widget<State> {
@@ -59,6 +96,7 @@ fn ui_builder() -> impl Widget<State> {
     let text =
         LocalizedString::new("hello-counter").with_arg("count", |data: &State, _env| data.day.into());
     let label = Label::new(text).padding(5.0).center();
+    let label2 = Label::new(|data: &State, _env: &_| format!("{}", data.day_data)).padding(5.0).center();
     let button = Button::new("increment")
         .on_click(|_ctx, data: &mut State, _env| {
             data.day += 1;
@@ -66,7 +104,7 @@ fn ui_builder() -> impl Widget<State> {
         })
         .padding(5.0);
 
-    Flex::column().with_child(label).with_child(button)
+    Flex::column().with_child(label).with_child(button).with_child(label2)
 }
 
 fn get_data(day: u32) -> Result<Day, Error> {
