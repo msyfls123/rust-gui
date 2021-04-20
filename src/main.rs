be1bbdfdf6e75d66d46f2ad312b4b0f12fa285a2
msyfls123 <msyfls123@gmail.com>
2021-04-21 00:53:21 +0800

    make it work with tokio

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,11 @@ use druid::{
 };
 use reqwest::Error;
 use serde::{Deserialize};
-use std::sync::mpsc::{Sender, channel, Receiver};
+// use std::sync::mpsc::{Sender, channel, Receiver};
 use std::thread;
+use tokio::sync::mpsc::{UnboundedSender, unbounded_channel, UnboundedReceiver};
+use tokio::sync::{Mutex};
+use std::sync::{Arc};
 
 #[derive(Deserialize, Debug, Clone)]
 #[serde(untagged)]
@@ -32,7 +35,7 @@ const DAY_DATA: Selector<String> = Selector::new("day_data");
 struct State {
     day: u32,
     #[data(ignore)]
-    dispatch: Sender<u32>,
+    dispatch: UnboundedSender<u32>,
     day_data: String,
 }
 
@@ -56,14 +59,16 @@ impl AppDelegate<State> for Delegate {
     }
 }
 
-// #[tokio::main]
-fn main() -> Result<(), PlatformError> {
-    let (tx, rx) = channel();
+#[tokio::main]
+async fn main() -> Result<(), PlatformError> {
+    let (tx, rx) = unbounded_channel();
+    let arc_rx = Arc::new(Mutex::new(rx));
     let main_window = WindowDesc::new(ui_builder);
     let data = 0_u32;
     let launcher = AppLauncher::with_window(main_window);
     let event_sink = launcher.get_external_handle();
-    request_day(rx, event_sink);
+    let arc_event_sink = Arc::new(Mutex::new(event_sink));
+    request_day(arc_rx, arc_event_sink);
     launcher.use_simple_logger()
         .delegate(Delegate {})
         .launch(State {
@@ -71,23 +76,29 @@ fn main() -> Result<(), PlatformError> {
             dispatch: tx.clone(),
             day_data: String::from(""),
         })
-
 }
 
-fn request_day(rx: Receiver<u32>, event_sink: druid::ExtEventSink) {
-    thread::spawn(move || {
-        while let res = rx.recv() {
+fn request_day(rx: Arc<Mutex<UnboundedReceiver<u32>>>, event_sink: Arc<Mutex<druid::ExtEventSink>>) {
+    println!("aaaa");
+    let cloned = rx.clone();
+    tokio::spawn(async move {
+        let mut locked = cloned.lock().await;
+        while let res = locked.recv().await {
             match res {
-                Ok(day) => {
+                Some(day) => {
                     println!("{}", day);
-                    let res = get_data(day).unwrap();
-                    thread::sleep(time::Duration::from_millis(1000));
-                    let day_data = format!("{:?}", res);
-                    event_sink
-                        .submit_command(DAY_DATA, day_data, Target::Auto)
-                        .expect("uhhh");
+                    let cloned_sink = event_sink.clone();
+                    tokio::spawn(async move {
+                        let res = get_data(day).await.unwrap();
+                        thread::sleep(time::Duration::from_millis(1000));
+                        let day_data = format!("{:?}", res);
+                        cloned_sink.lock().await
+                            .submit_command(DAY_DATA, day_data, Target::Auto)
+                            .expect("uhhh");
+                    });
+                    
                 },
-                Err(err) => eprintln!("{:?}", err),
+                _ => eprintln!("empty"),
             }
         }
     });
@@ -102,8 +113,9 @@ fn ui_builder() -> impl Widget<State> {
     let button = Button::new("increment")
         .on_click(|_ctx, data: &mut State, _env| {
             data.day += 1;
+            eprintln!("emmm {}", data.day);
             if data.dispatch.send(data.day).is_err() {
-                eprintln!("dispatch err");
+                eprintln!("emmm");
             };
         })
         .padding(5.0);
@@ -111,9 +123,9 @@ fn ui_builder() -> impl Widget<State> {
     Flex::column().with_child(label).with_child(button).with_child(label2)
 }
 
-fn get_data(day: u32) -> Result<Day, Error> {
+async fn get_data(day: u32) -> Result<Day, Error> {
     let url = format!("https://day.ebichu.cc/api/{}", day);
-    let response = reqwest::blocking::get(&url)?;
-    let day: Day = response.json()?;
+    let response = reqwest::get(&url).await?;
+    let day: Day = response.json().await?;
     Ok(day)
 }
