807ae2725dee09640cea3968a9488b76b3746309
msyfls123 <msyfls123@gmail.com>
2021-05-08 00:09:46 +0800

    try axis alignment

--- src/components/dummy_window.rs
+++ src/components/dummy_window.rs
@@ -1,17 +1,20 @@
 use druid::{Widget, Color, WidgetExt};
 use druid::widget::{
   Flex, Label,
-  FlexParams, CrossAxisAlignment,
+  FlexParams, CrossAxisAlignment, MainAxisAlignment,
 };
 
 use crate::types::{State};
 
 pub fn dummy_window_builder() -> impl Widget<State> {
   Flex::row()
+    .main_axis_alignment(MainAxisAlignment::SpaceAround)
+    .must_fill_main_axis(true)
     .with_flex_child(Label::new("dummy")
+      .expand_height()
       .padding(20.0)
       .border(Color::RED, 2.0),
-      FlexParams::new(0.5, CrossAxisAlignment::End)
+      FlexParams::new(2.0, CrossAxisAlignment::End)
     )
     .with_child(Label::new("code")
       .padding(20.0)
